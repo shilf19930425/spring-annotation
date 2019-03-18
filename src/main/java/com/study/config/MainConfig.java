f43faa71af528c1a744f0851d0de73c487bea667
shilf <13099051151@163.com>
2019-03-18 22:44:11 +0800

    错误修改

--- src/main/java/com/study/config/MainConfig.java
+++ src/main/java/com/study/config/MainConfig.java
@@ -1,10 +1,8 @@
 package com.study.config;
 
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScans()
 public class MainConfig
 {
 
