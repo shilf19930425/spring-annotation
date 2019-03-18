642b9e0f3ba3fe62102db5896bdc81973b6e68ad
shilf <13099051151@163.com>
2019-03-18 22:49:30 +0800

    ..

--- src/main/java/com/study/config/MainConfig.java
+++ src/main/java/com/study/config/MainConfig.java
@@ -1,9 +1,18 @@
 package com.study.config;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.study.bean.Person;
+
 @Configuration
+@ComponentScan(value = "com.study")
 public class MainConfig
 {
-
+	public static void main(String[] args)
+	{
+		ApplicationContext applContext = new AnnotationConfigApplicationContext(Person.class);
+	}
 }
